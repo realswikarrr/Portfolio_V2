4bd28532700b02b9561f474a6a6719c86ae1aef1
Swikar Adhikari <swikara082@gmail.com>
2023-07-10 09:16:00 +0930

    feat: 💭 layout is done

---
+++ src/components/BlurCircle.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react";
+import type { TailwindColor } from "@/utils/types/tailwind";
+import { MAP_COLOR_VARIANT_TO_BG } from "@/utils/mapVariants";
+import clsx from "clsx";
+
+interface BlurCircleProps {
+  position: "bottom" | "top";
+  color: TailwindColor;
+}
+
+const BlurCircle: FC<BlurCircleProps> = ({ position, color }) => {
+  return (
+    <div
+      className={clsx(
+        `fixed z-0 h-[134px] w-[134px] lg:w-[300px] lg:h-[300px] rounded-full ${MAP_COLOR_VARIANT_TO_BG[color]} blur-[150px] md:blur-[350px] opacity-50`,
+        position === "top" && "left-0 top-0",
+        position === "bottom" && "right-0 bottom-0"
+      )}
+    ></div>
+  );
+};
+
+export default BlurCircle;
